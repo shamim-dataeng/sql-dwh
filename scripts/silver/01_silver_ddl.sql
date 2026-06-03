df77720b979be68cd633f8ccc71035b99fd012a1
shamim-dataeng <datapulse2804@gmail.com>
2026-06-03 17:10:20 +0400

    feat: restructure scripts and ddl for silver layer

---
+++ scripts/silver/01_silver_ddl.sql
@@ -0,0 +1,67 @@
+-- Silver layer(6 tables): it can include metadata columns too added by data engineers like create_date,update_date,source_system,file_location
+
+-- Metadata column added here is dwh_create_date which will capture the date and time when the record is inserted into the silver layer. This can be useful for tracking data lineage, auditing, and understanding when data was processed and loaded into the silver layer.
+
+-- CRM  tables  
+drop table if exists silver_crm_cust_info;
+create table silver_crm_cust_info(
+	cst_id int,
+    cst_key varchar(50),
+    cst_firstname varchar(50),
+    cst_lastname varchar(50),
+    cst_marital_status varchar(50),
+    cst_gndr varchar(50),
+    cst_create_date date,
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+drop table if exists silver_crm_prd_info;
+create table silver_crm_prd_info(
+	prd_id int,
+    prd_key varchar(50),
+    prd_nm varchar(50),
+    prd_cost int,
+    prd_line varchar(50),
+    prd_start_dt date,
+    prd_end_dt date,
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+drop table if exists silver_crm_sales_details;
+create table silver_crm_sales_details(
+	sls_ord_num varchar(50),
+    sls_prd_key varchar(50),
+    sls_cust_id int,
+    sls_order_dt int,
+    sls_ship_dt int,
+    sls_due_dt int,
+    sls_sales int,
+    sls_quantity int,
+    sls_price int,
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- ERP tables  
+
+drop table if exists silver_erp_cust_az12;
+create table silver_erp_cust_az12(
+	cid varchar(50),
+    bdate date,
+    gen varchar(50),
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+drop table if exists silver_erp_loc_a101;
+create table silver_erp_loc_a101(
+	cid varchar(50),
+    cntry varchar(50),
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+drop table if exists silver_erp_px_cat_g1v2;
+create table silver_erp_px_cat_g1v2(
+	id varchar(50),
+    cat varchar(50),
+    subcat varchar(50),
+    maintenance varchar(50),
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
+);
